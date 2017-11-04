0be3879e11a4a5f869983daebc8c5eb0ed7d7eb5
OsciiArt <spnxxxx@gmail.com>
2017-11-04 11:55:33 +0900

    beta version of DeepAA2

--- README.md
+++ README.md
@@ -1,62 +1,31 @@
 DeepAA
 ====
 
-本レポジトリについての記事を書きました。
-よろしければご覧ください。
+This is convolutional neural networks generating ASCII art.
+This repositry is under the construction.
 
-[ディープラーニングでアスキーアートを作る](http://qiita.com/OsciiArt/items/325714d8ab3f2b482ced)
+![image sample](https://github.com/OsciiArt/DeepAA/output/sample.png)
 
-This is an article about this repository. Sorry, it's only in Japanese, but you can see some sample outputs here.
+## Requirements
 
-## 概要
-ディープラーニング技術を用いて画像をアスキーアートに変換します。
++ TensorFlow (1.3.0)
++ Keras (2.0.8)
++ NumPy (1.13.3)
++ Pillow (4.2.1)
++ Pandas (0.18.0)
++ Scikit-learn (0.19.0)
 
-## 前提
-動作にはPythonの実行環境が必要です。
-
-以下の環境でテスト済みです。
-
-+ Python 3.5 (64bit)
-
-## 依存パッケージ
-本ツールは以下のパッケージに依存しています。
-
-+ TensorFlow
-+ Keras
-+ NumPy
-+ Pillow (PIL Fork)
-
-## 使用方法
-`deepaa_output.py ` の17行目
+## How to use
+please change the line 15 of `output.py `
 
 ```
-image_path = 'test_image.png' # 変換する画像を指定
+image_path = 'sample images/original images/21 original.png' # put the path of the image that you convert.
 ```
-の画像パスを任意のものに置き換えて実行してください。
-画像は白黒の線画を用いてください。
-細線化された画像の方が良い結果が得られやすいです。
+into the path of image file that you use.
+You should use a grayscale line image.
 
-現段階では学習済みモデルによる出力を行うコードのみ公開しています。
-学習データの前処理、学習のコードは今後公開の予定です。
+then run `output.py `.
+converted images will be output at `output/ `.
 
-## 実行例
-![実行例](http://i.imgur.com/rasaYQi.png)
-
-## 学習
-注) 現在アップしているデータセットは実際に学習に用いたものではありません
-### Steps
-1. **データの前処理**
-    ```bash
-    python process_data.py
-    ```
-    アスキーアートのデータを保存しているテキストファイル (data/aa_sample.ast) を学習データに変換します。データはdata/traindata/に出力されます。
-    <br>
-
-2. モデルの学習
-    ```bash
-    python deepaa_train.py
-    ```
-    モデルの学習を実行します。学習したモデルはmodel/model_test/に保存されます。
-
-## ライセンス
-本ライブラリのライセンスは `MIT License` になります。
+## License
+The pre-trained models and the other files we have provided are licensed under the MIT License.
---
+++ train.py
@@ -0,0 +1,219 @@
+from keras.models import Model
+from keras.layers import Dense, Activation, Reshape, Dropout, Embedding, Input, BatchNormalization
+from keras.layers import Concatenate, Multiply, Conv2D, MaxPooling2D, Add, Flatten, GaussianNoise
+from keras.models import model_from_json
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint, \
+    EarlyStopping, CSVLogger, ReduceLROnPlateau
+
+import time
+import numpy as np
+
+np.random.seed(42)
+import pandas as pd
+from os import path
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import math
+from keras.optimizers import Adam
+from data_utils import *
+from multiprocessing import Pool
+
+
+def CBRD(inputs, filters=64, kernel_size=(3,3), droprate=0.5):
+    x = Conv2D(filters, kernel_size, padding='same',
+               kernel_initializer='random_normal')(inputs)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    # x = Dropout(droprate)(x)
+    return x
+
+
+def DBRD(inputs, units=4096, droprate=0.5):
+    x = Dense(units)(inputs)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Dropout(droprate)(x)
+    return x
+
+def CNN(input_shape=None, classes=1000):
+    inputs = Input(shape=input_shape)
+
+    # Block 1
+    x = GaussianNoise(0.3)(inputs)
+    x = CBRD(x, 64)
+    x = CBRD(x, 64)
+    x = MaxPooling2D()(x)
+
+    # Block 2
+    x = CBRD(x, 128)
+    x = CBRD(x, 128)
+    x = MaxPooling2D()(x)
+
+    # Block 3
+    x = CBRD(x, 256)
+    x = CBRD(x, 256)
+    x = CBRD(x, 256)
+    x = MaxPooling2D()(x)
+
+    # Classification block
+    x = Flatten(name='flatten')(x)
+    x = DBRD(x, 4096)
+    x = DBRD(x, 4096)
+    x = Dense(classes, activation='softmax', name='predictions')(x)
+
+    model = Model(inputs=inputs, outputs=x)
+
+    return model
+
+
+def add_mergin(img, mergin):
+    if mergin!=0:
+        img_new = np.ones([img.shape[0] + 2 * mergin, img.shape[1] + 2 * mergin], dtype=np.uint8) * 255
+        img_new[mergin:-mergin, mergin:-mergin] = img
+    else:
+        img_new = img
+    return img_new
+
+
+def load_img(args):
+    img_path, x, y, input_size, mergin, slide = args
+    img = np.array(Image.open(img_path))
+    if len(img.shape) == 3:
+        img = img[:, :, 0]
+    img = add_mergin(img, mergin)
+    x += np.random.randint(-slide, slide+1)
+    y += np.random.randint(-slide, slide+1)
+    img = img[y:y + input_size, x:x + input_size]
+    img = img.reshape([1, input_size, input_size, 1])
+    # print(img_path, x, y, input_size, mergin )
+    # print(input_size, img.shape)
+    return img
+
+def batch_generator(df, img_dir, input_size, batch_size, num_label, slide,
+                    tail='line', shuffle=True):
+    df = df.reset_index()
+    batch_index = 0
+    mergin = (input_size - 18) // 2 + 30
+    n = df.shape[0]
+    pool = Pool()
+    while 1:
+        if batch_index == 0:
+            index_array = np.arange(n)
+            if shuffle:
+                index_array = np.random.permutation(n)
+
+        current_index = (batch_index * batch_size) % n
+        if n >= current_index + batch_size:
+            current_batch_size = batch_size
+            batch_index += 1
+        else:
+            current_batch_size = n - current_index
+            batch_index = 0
+
+        index_array_batch = index_array[current_index: current_index + current_batch_size]
+        batch_img_path = df['file_name'][index_array_batch].apply(
+            lambda x: img_dir + x + tail + '.png').as_matrix()
+        # print(batch_img_path)
+        batch_coord_x = (df['x'][index_array_batch] + 30).as_matrix()
+        batch_coord_y = (df['y'][index_array_batch] + 30).as_matrix()
+        # print(batch_img_path[0], batch_coord_x[0], batch_coord_y[0], mergin)
+        batch_x = pool.map(load_img,
+                           [(batch_img_path[i],
+                             batch_coord_x[i],
+                             batch_coord_y[i],
+                             input_size,
+                             mergin,
+                             slide)
+                           for i in range(current_batch_size)])
+        # print(batch_x[0].shape)
+        batch_x = np.concatenate(batch_x, axis=0)
+        batch_x = batch_x.astype(np.float32) / 255
+        # print(batch_x.shape)
+
+        batch_y = df['label'][index_array[current_index: current_index + current_batch_size]].as_matrix()
+        batch_y = np.eye(num_label)[batch_y]
+
+        yield batch_x, batch_y
+
+
+def train_generator(df, img_dir, input_size, batch_size, num_label, slide,
+                    tail='line', shuffle=True):
+    gen_line = batch_generator(df, img_dir, input_size,
+                               batch_size // 2, num_label, slide, tail="line_resize")
+    gen_orig = batch_generator(df, img_dir, input_size,
+                               batch_size // 2, num_label, slide, tail="orig")
+    while True:
+        batch1 = next(gen_line)
+        batch2 = next(gen_orig)
+        batch_x = np.concatenate([batch1[0], batch2[0]])
+        batch_y = np.concatenate([batch1[1], batch2[1]])
+        yield batch_x, batch_y
+
+
+def train():
+    # parameter
+    num_epoch = 256
+    batch_size = 64
+    input_shape = [64,64,1]
+    learning_rate = 0.001
+    df_path = "data/data_500.csv"
+    char_list_path = "data/char_list_500.csv"
+    img_dir = "data/image_selected/"
+
+    # load text
+    df = pd.read_csv(df_path, encoding="cp932")
+    char_list = pd.read_csv(char_list_path, encoding="cp932")
+    num_label = char_list[char_list['frequency']>=10].shape[0]
+    # print(num_label)
+    df = df[df['label']<num_label]
+    df = df.reset_index()
+    input_size = input_shape[0]
+    slide = 1
+    df_train, df_val = train_test_split(df, test_size=0.1, random_state=42)
+    gen = train_generator(df_train, img_dir,
+                          input_size, batch_size, num_label, slide)
+    gen_val = batch_generator(df_val, img_dir, input_size,
+                              batch_size, num_label, 0,
+                              tail="line_resize", shuffle=False)
+
+    # build model
+    model = CNN(input_shape=input_shape, classes=num_label)
+    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+    # train
+    nb_train = df_train.shape[0]
+    nb_val = df_val.shape[0]
+    nb_step = math.ceil(nb_train / batch_size)
+    nb_val_step = math.ceil(nb_val / batch_size)
+
+    format = "%H%M"
+    ts = time.strftime(format)
+    save_path = "model/" + path.splitext(__file__)[0] + "_" + ts
+
+    json_string = model.to_json()
+    with open(save_path + '_model.json', "w") as f:
+        f.write(json_string)
+
+    csv_logger = CSVLogger(save_path + '_log.csv', append=True)
+    check_path = save_path + '_e{epoch:02d}_vl{val_loss:.5f}.hdf5'
+    save_checkpoint = ModelCheckpoint(filepath=check_path, monitor='val_loss', save_best_only=True)
+    lerning_rate_schedular = ReduceLROnPlateau(patience=8, min_lr=learning_rate * 0.00001)
+    early_stopping = EarlyStopping(monitor='val_loss',
+                                   patience=16,
+                                   verbose=1,
+                                   min_delta=1e-4,
+                                   mode='min')
+    Callbacks = [csv_logger,
+                 save_checkpoint,
+                 lerning_rate_schedular, early_stopping]
+    model.fit_generator(gen,
+                        steps_per_epoch=nb_step,
+                        epochs=num_epoch,
+                        validation_data=gen_val,
+                        validation_steps=nb_val_step,
+                        callbacks=Callbacks
+                        )
+
+
+if __name__ == "__main__":
+    train()
